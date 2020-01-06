5e17a7ef260f9d94e0e72714ed21d44e0b8178c0
mihirp90 <mihirp90@gmail.com>
2020-01-07 00:12:43 +0530

    new changes

--- SprakMaven/src/main/scala/dfBasic/DFBasicOperations.scala
+++ SprakMaven/src/main/scala/dfBasic/DFBasicOperations.scala
@@ -8,13 +8,40 @@ object DFBasicOperations {
     val spark = SparkSession.builder().appName("Basic DF Operations").master("local")
       .getOrCreate()
 
-    val dataDF = spark.read.csv("src/main/resources/datasets/calendar.csv")
+    val dataDF = spark.read.option("header", "true").
+      option("inferSchema", "true").csv("src/main/resources/datasets/calendar.csv")
     dataDF.printSchema()
 
     //op1
     val dataSchema = dataDF.schema //return schema as struct type
 
     println(dataSchema)
+
+    println(dataDF.columns.mkString(","))
+
+    val columnDesc = dataDF.describe("listing_id")
+    columnDesc.show()
+
+    val colAndType = dataDF.dtypes
+    colAndType.foreach(println)
+
+    //selecting only desired columns
+
+    dataDF.select("listing_id", "price").show(10)
+
+    //using where clause
+    dataDF.select("listing_id", "price").where("price=='$63.00'").
+      show(10)
+
+    //or using filter
+    dataDF.select("listing_id", "price").filter(dataDF("price") === "$63.00").
+      show(10)
+
+    //groupby with count
+    dataDF.select("listing_id", "price").groupBy("price").count().
+      show()
+
+
   }
 
 }
